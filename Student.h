e47f0b9d5af21604dfdf30a29c3056dfdc17652c
James Boyce <61768977+jdbsquire@users.noreply.github.com>
2025-11-10 03:12:47 +0000

    Initial commit of a few files

---
+++ Student.h
@@ -0,0 +1,35 @@
+#ifndef STUDENT_H
+#define STUDENT_H
+
+#include <string>
+using namespace std;
+
+class Student {
+private:
+    string name;
+    string studentID;
+    string phone;
+    double gpa;
+
+    static double highestGPA;
+
+public:
+    Student();
+    Student(const string& name, const string& id, const string& phone, double gpa);
+
+    // Getters
+    string getName() const;
+    string getStudentID() const;
+    string getPhone() const;
+    double getGPA() const;
+
+    // Setters
+    void setName(const string& n);
+    void setStudentID(const string& id);
+    void setPhone(const string& p);
+    void setGPA(double g);
+
+    static double getHighestGPA();
+};
+
+#endif
